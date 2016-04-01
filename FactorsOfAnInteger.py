4efa781193cd1da60dfa8b8ea4d2196cdc9a969c
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-01 19:31:19 -0400

    Adding FactorsOfAnInteger with the is_factor function implemented

--- FactorsOfAnInteger.py
+++ FactorsOfAnInteger.py
@@ -1,3 +1,4 @@
+# A function that looks if a is a factor of b then returns True or False
 def is_factor(a, b):
 	if b % a == 0:
 		return True
@@ -6,5 +7,19 @@ def is_factor(a, b):
 
 print 'Is 4 a factor of 1024?',
 print is_factor(4, 1024)
-print 'Is 322 a facor of 50134435?', 
+print 'Is 322 a facor of 50134435?',
 print is_factor(322, 50134435)
+
+# A function that prints all positive factors of b
+def factors(b):
+	for i in range(1, b+1):
+		if b % i == 0:
+			print(i)
+
+b = input('Your Number Please: ')
+b = float(b)
+
+if b > 0 and b.is_integer():
+	factors(int(b))
+else:
+	print('Please enter a positive integer')
