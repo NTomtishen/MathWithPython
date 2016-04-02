4b9bd575f9527f31457e6b22823332784a7d6aa1
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-02 19:49:45 -0400

    Adds EvenOddVendingMachine.py

---
+++ EvenOddVendingMachine.py
@@ -0,0 +1,13 @@
+num = float(input('Please enter a number: '))
+if num.is_integer():
+	num = int(num)
+	if num % 2 == 0:
+		print 'The number you gave is even.'
+		for item in range(10):
+			print num + item*2
+	if num % 2 != 0:
+		print 'The number you gave is odd.'
+		for item in range(10):
+			print num + item*2
+else:
+	print 'You entered an invalid number'
