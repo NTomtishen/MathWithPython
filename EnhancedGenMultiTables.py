de483d32908cecf0552a7d00d5500e9369fbd3af
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-03 19:31:04 -0400

    Adds the ability for the user to choose when to exit in EnhancedGenMultiTables.py

--- EnhancedGenMultiTables.py
+++ EnhancedGenMultiTables.py
@@ -2,6 +2,10 @@ def multi_table(a, b):
 	for i in range(1, b+1):
 		print('{0} x {1} = {2}'.format(a, i, a*i))
 
-a = input('Enter a number: ')
-b = input('Enter how many multiples you want: ')
-multi_table(float(a), int(b))
+while True:
+	a = input('Enter a number: ')
+	b = input('Enter how many multiples you want: ')
+	multi_table(float(a), int(b))
+	answer = raw_input('Would you like to exit? (y) for yes ')
+	if answer == 'y':
+		break
