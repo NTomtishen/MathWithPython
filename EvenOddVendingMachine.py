88d29c73471961d34df0a4fd230c82c99e200bf3
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-03 19:29:37 -0400

    Adds the ability for the user to choose when to exit in EvenOddVendingMachine.py

--- EvenOddVendingMachine.py
+++ EvenOddVendingMachine.py
@@ -1,13 +1,17 @@
-num = float(input('Please enter a number: '))
-if num.is_integer():
-	num = int(num)
-	if num % 2 == 0:
-		print 'The number you gave is even.'
-		for item in range(10):
-			print num + item*2
-	if num % 2 != 0:
-		print 'The number you gave is odd.'
-		for item in range(10):
-			print num + item*2
-else:
-	print 'You entered an invalid number'
+while True:
+	num = float(input('Please enter a number: '))
+	if num.is_integer():
+		num = int(num)
+		if num % 2 == 0:	
+			print 'The number you gave is even.'
+			for item in range(10):
+				print num + item*2
+		if num % 2 != 0:
+			print 'The number you gave is odd.'
+			for item in range(10):
+				print num + item*2
+	else:
+		print 'You entered an invalid number'
+	answer = raw_input('Would you like to exit? (y) for yes ')
+	if answer == 'y':
+		break
