a4f895bb51acc451350467e893688d9f51d7f39a
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-07 10:56:09 -0400

    Allows for user input in FactorsOfAnInteger.py

--- FactorsOfAnInteger.py
+++ FactorsOfAnInteger.py
@@ -5,21 +5,22 @@ def is_factor(a, b):
 	else:
 		return False
 
-print 'Is 4 a factor of 1024?',
-print is_factor(4, 1024)
-print 'Is 322 a facor of 50134435?',
-print is_factor(322, 50134435)
-
 # A function that prints all positive factors of b
 def factors(b):
 	for i in range(1, b+1):
 		if b % i == 0:
 			print(i)
 
-b = input('Your Number Please: ')
+a = input('Enter your first number: ')
+b = input('Enter the second number: ')
+a = float(a)
 b = float(b)
 
+print is_factor(a, b)
+
+'''
 if b > 0 and b.is_integer():
 	factors(int(b))
 else:
 	print('Please enter a positive integer')
+'''
