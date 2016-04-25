cc86939e20247da2cfd48c992ea25b348c19cc9e
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-04-25 09:59:35 -0400

    Adds AverAnnTemp.py, MonthlyTempTrends.py, SimplePyplot.py and FormulaPlotting.py as well as modifies Graphs.py

--- Graphs.py
+++ Graphs.py
@@ -2,6 +2,5 @@ from pylab import plot, show
 
 x_numbers = [1, 2, 3]
 y_numbers = [2, 4, 6]
-plot(x_numbers, y_numbers)
-plot(x_numbers, y_numbers, marker = 'o')
+plot(x_numbers, y_numbers, 'o')
 show()
