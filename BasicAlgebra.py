ddb40e661454a69357e2a34543c14affe88cdbc6
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-05 10:06:07 -0400

    Adds SympyPlotting.py

--- BasicAlgebra.py
+++ BasicAlgebra.py
@@ -22,6 +22,7 @@ expr = sympify(expr)
 print expr
 '''
 
+'''
 x = Symbol('x')
 a = Symbol('a')
 b = Symbol('b')
@@ -30,3 +31,24 @@ expr = a*x*x + b*x + c
 pprint(expr)
 
 pprint(solve(expr, x, dict=True))
+'''
+
+'''
+s = Symbol('s')
+u = Symbol('u')
+t = Symbol('t')
+a = Symbol('a')
+expr = u*t + (1/2)*a*t*t - s
+t_expr = solve(expr,t, dict=True)
+pprint(t_expr)
+'''
+
+x = Symbol('x')
+y = Symbol('y')
+expr1 = 2*x + 3*y - 6
+expr2 = 3*x + 2*y - 12
+soln = solve((expr1, expr2), dict=True)
+print soln
+soln = soln[0]
+print expr1.subs({x:soln[x], y:soln[y]})
+print expr2.subs({x:soln[x], y:soln[y]})
