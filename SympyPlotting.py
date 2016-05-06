95290c23f4a7aa12c2ccccac7ad961fa7659e7e4
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-06 10:04:05 -0400

    Worked on SympyPlotting.py

--- SympyPlotting.py
+++ SympyPlotting.py
@@ -1,4 +1,27 @@
 from sympy.plotting import plot
-from sympy import Symbol
+from sympy import *
+# p = plot((2*x+3), (x, -5, 5), title = 'A Line', xlabel = 'x', ylabel = '2x + 3')
+
 x = Symbol('x')
-plot((2*x+3), (x, -5, 5), title = 'A Line', xlabel = 'x', ylabel = '2x + 3')
+p = plot(2*x+3, 3*x+1, legend=True, show=False)
+p[0].line_color = 'b'
+p[1].line_color = 'r'
+p.show()
+
+'''
+def plot_expression(expr):
+	solutions = (solve(expr, y))
+	expr_y = solutions[0]
+	plot(expr_y)
+
+
+expr = raw_input('Enter your expression in terms of x and y: ')
+x = Symbol('x')
+y = Symbol('y')
+try:
+	expr = sympify(expr)
+except SympifyError:
+	print('Invalid input')
+else:
+	plot_expression(expr)
+'''
