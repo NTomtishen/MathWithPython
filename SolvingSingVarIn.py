9767aeac8f56e5e137f4315fc136f5d3f3a3294d
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-16 09:30:54 -0400

    Finishes SolvingSingVarIn.py by adding the isolve function and implementing it.

--- SolvingSingVarIn.py
+++ SolvingSingVarIn.py
@@ -1,21 +1,22 @@
 from sympy import *
 
-# Method to solve polynomial inequalities
-'''
-x = Symbol('x')
-ineq_obj = -x**2 + 4 < 0
-lhs = ineq_obj.lhs
-p = Poly(lhs, x)
-rel = ineq_obj.rel_op
-print solve_poly_inequality(p, rel)
-'''
-
-# Method to solve rational inequalities
-x = Symbol('x')
-ineq_obj = ((x-1)/(x+2)) > 0
-lhs = ineq_obj.lhs
-numer, denom = lhs.as_numer_denom()
-p1 = Poly(numer)
-p2 = Poly(denom)
-rel = ineq_obj.rel_op
-print solve_rational_inequalities([[((p1, p2), rel)]])
+def isolve():
+	x = Symbol('x')
+	ineq = input('Enter your inequality: ')
+	ineq = sympify(ineq)
+	ineq_obj = ineq
+	if ineq.is_polynomial():
+		lhs = ineq_obj.lhs
+		p = Poly(lhs, x)
+		rel = ineq_obj.rel_op
+		print solve_poly_inequality(p, rel)
+	elif ineq.is_rational_function():
+		lhs = ineq_obj.lhs
+		numer, denom = lhs.as_numer_denom()
+		p1 = Poly(numer)
+		p2 = Poly(denom)
+		rel = ineq_obj.rel_op
+		print solve_rational_inequalities([[((p1, p2), rel)]])
+	else:
+		print solve_univariate_inequality(ineq_obj, x, relational = False)
+isolve()
