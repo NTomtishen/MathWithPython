31f01fffae548ef078c1ba39f7d49160676011c8
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-17 10:00:25 -0400

    Adds Pendulum.py

--- BasicSets.py
+++ BasicSets.py
@@ -1,10 +1,7 @@
 from sympy import FiniteSet
 from fractions import Fraction
-members = [2, 4, 6, Fraction(1, 5)]
-s = FiniteSet(*members)
-print s
-print len(s)
-print 4 in s
-print 15 in s
-for member in s:
-	print member
+s = FiniteSet(1, 2, 3)
+t = FiniteSet(2, 4, 6, 8)
+p = s**3
+for elem in p:
+	print elem
