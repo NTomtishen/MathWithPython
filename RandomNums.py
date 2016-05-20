1d4b7dc13d20ebd3a3e9958147fda0e441959ede
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-20 10:40:31 -0400

    Adds TargetScorePossible.py

--- RandomNums.py
+++ RandomNums.py
@@ -1,2 +1,23 @@
 import random
+'''
 print random.randint(1, 6)
+'''
+print random.random()
+'''
+def toss():
+	if random.random() < 2/3:
+		return 0
+	else:
+		return 1
+
+tails = 0
+heads = 0
+total = heads + tails
+for item in range(0, 100):
+	i = toss()
+	if i == 1:
+		tails += 1
+	else:
+		heads += 1
+print tails, heads, total
+'''
