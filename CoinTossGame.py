fc2de7758b6658ba545cc5527eb5061c1328780b
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-05-27 10:48:47 -0400

    Modified the program to create a function to flip the coin.

--- CoinTossGame.py
+++ CoinTossGame.py
@@ -1,20 +1,32 @@
 import random
-cash = input('Enter your starting amount: ')
-times = 0
-max = [cash, 0]
-while cash > 0:
-	# Heads is 1, Tails is 2
-	coin = random.randint(1, 2)
-	if coin == 1:
-		cash += 1
-		print 'Heads!',
-	elif coin == 2:
-		cash -= 1.5
-		print 'Tails',
-	print ('Current amount: %s' % (cash))
-	times += 1
-	if cash > max[0]:
-		max[0] = cash
-		max [1] = times
-print 'Game Over! :( Current amount: %s. Coin tosses: %s' % (cash, times)
-print 'You reached your max amount, %s, after %s toss(es)' % (max[0], max[1])
+#cash = input('Enter your starting amount: ')
+
+
+def coinFlips(cash):
+	times = 0
+	#max = [cash, 0]
+	while cash > 0:
+		# Heads is 1, Tails is 2
+		coin = random.randint(1, 2)
+		if coin == 1:
+			cash += 1
+			#print 'Heads!',
+		elif coin == 2:
+			cash -= 1.5
+			#print 'Tails!',
+		#print ('Current amount: %s' % (cash))
+		times += 1
+		#if cash > max[0]:
+		#	max[0] = cash
+		#	max [1] = times
+	return times
+	#print 'Game Over! :( Current amount: %s. Coin tosses: %s' % (cash, times)
+	#print 'You reached your max amount, %s, after %s toss(es)' % (max[0], max[1])
+
+tosses = 0.0
+trials = 0.0
+for i in range(0, 1000000):
+	tosses += coinFlips(5)
+	trials += 1
+
+print tosses, trials, tosses/trials
