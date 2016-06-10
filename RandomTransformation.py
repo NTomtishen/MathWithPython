69f99988826f015ca348745d91281f7f799f333c
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-06-10 09:22:34 -0400

    Modified RandomTransformation.py to allow transformation in any direction.

--- RandomTransformation.py
+++ RandomTransformation.py
@@ -11,8 +11,18 @@ def transformation_2(p):
     y = p[1]
     return x + 1, y + 1
 
+def transformation_3(p):
+    x = p[0]
+    y = p[1]
+    return x - 1, y + 1
+
+def transformation_4(p):
+    x = p[0]
+    y = p[1]
+    return x - 1, y - 1
+
 def transform(p):
-    transformations = [transformation_1, transformation_2]
+    transformations = [transformation_1, transformation_2, transformation_3, transformation_4]
     t = random.choice(transformations)
     x, y = t(p)
     return x, y
@@ -26,10 +36,12 @@ def build_trajectory(p, n):
         y.append(p[1])
     return x, y
 
-p = (1, 1)
+p = (0, 0)
 n = int(input('Enter the number of iterations: '))
 x, y = build_trajectory(p, n)
 plt.plot(x, y)
 plt.xlabel('X')
 plt.ylabel('Y')
+ax = plt.axes()
+ax.set_aspect('equal')
 plt.show()
